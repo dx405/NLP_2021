380b3688f6c5f6fd2d9521baf9481d3548e47c13
unknown <giovanniciccia1@gmail.com>
2021-04-21 19:46:36 -0400

    Added more stopwords and ignored names

--- bag_of_words.py
+++ bag_of_words.py
@@ -4,6 +4,16 @@ from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 import re
 lemmatizer = WordNetLemmatizer()
+other_stopwords = ["one", "two", "also", "novel", "story", "life","find", "new", "home","book",
+                     "first", "man","woman", "back", "day", "time", "father","take","away","old", 
+                     "return", "see", "tell", "begin", "god", "go", "death", "ship","young",
+                     "make", "family", "end", "come", "human", "world", "escape", "try", "becomes",
+                     "later", "friend", "would", "love", "people", "work",
+                     "attempt", "meet", "however", "help", "way", "year", "men",
+                     "another", "become", "next", "son", "three", "give", "house",
+                     "set", "attack", "town", "name", "kill", "order", "call", "turn",
+                     "must", "even", "child", "plan", "get", "use",
+                     "leave", "n't", "place"]
 data = json.load(open("data.json"))
 
 tokens_list = []
@@ -12,13 +22,18 @@ for summary in data:
     tokens = word_tokenize(summary["summary"])
     clean_tokens = []
     for token in tokens:
+        lemma = lemmatizer.lemmatize(token)
+        if token[0].isupper():
+            continue
         if token.lower() in stopwords.words('english'):
             continue
-        if bool(re.match(r"[.,()?!\"\'-;]|``",token)):
+        if lemma.lower() in other_stopwords:
             continue
-        clean_tokens.append(lemmatizer.lemmatize(token))
+        if bool(re.match(r"[.,()?!\"\'-;'&]|``",token)):
+            continue
+        clean_tokens.append(lemma)
     tokens_list.append(clean_tokens)
-    for token in tokens:
+    for token in clean_tokens:
         if token in word_count:
             word_count[token] += 1
         else:
@@ -41,8 +56,6 @@ for i in range(len(tokens_list)):
     tokens = tokens_list[i]
     bag = {}
     for token in tokens:
-        if token.lower() in stopwords.words('english'):
-            continue
         if token not in top_words:
             continue
         if token in bag:
@@ -56,6 +69,9 @@ j = json.dumps(bags)
 with open("word_frequencies.json",'w') as f:
     f.write(j)
     f.close()
+
+for word in top_words:
+    print(word + " " + str(word_count[word]))
         
 
 

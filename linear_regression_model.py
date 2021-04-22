ddcf235d261f40988931d31fa67e1b792c3defd8
Diane Xie <dx405@nyu.edu>
2021-04-21 22:33:17 -0400

    started linear regression model

---
+++ linear_regression_model.py
@@ -0,0 +1,40 @@
+import json
+import numpy as np
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+
+def setup_data():
+
+	data = json.load(open('word_frequencies.json'))
+	# set up data
+	genre, words = [], []
+
+	for i in data:
+		# iterate through genres
+		for genres in i[0]:
+			genre.append(genres)
+			words.append(list(i[1].keys()))
+	
+	# convert to numpy arrays
+	words = np.zeros([len(words), len(max(words, key= lambda x: len(x)))])
+	# array of sequence error with words
+	genre = np.array(genre, dtype=object)
+
+	return words, genre
+
+
+# get data
+X, Y = setup_data()
+
+# split into training and testing 
+x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=.20)
+
+# train linear regression model
+reg = LinearRegression().fit(x_train, y_train)
+
+# performing prediction on dataset
+y_pred = reg.predict(x_test)
+
+# return classification report
+print(classification_report(y_test, y_pred))
